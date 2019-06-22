bc1741eaa74b713c073587e4c7cbdc9a2b629e49
amaly-paranavithana <amaly.paranavithana@laposte.net>
2019-06-23 01:12:12 +0200

    Validation des méthodes insert, select by id et select all. Méthode update à ajouter. Méthode delete à corriger => IllegalArgumentException : removing a detached instance

--- src/test/java/testApp/TestApp.java
+++ src/test/java/testApp/TestApp.java
@@ -8,39 +8,44 @@ import metier.*;
 public class TestApp {
 
 	public static void main(String[] args) {
-		insertBaguette();
-		// selectBaguetteById();
-		// selectAllBaguette();
-		//deleteBaguette();
+		//insertBaguette();
+		 //selectBaguetteById();
+		 //selectAllBaguette();
+		deleteBaguette();
 	}
 
 	static DaoBaguette daoB = new DaoBaguette();
 
-//N'affiche pas la valeur d l'enum
+//OK
 	public static void insertBaguette() {
-		Baguette hG = new Baguette(Bois.valueOf("Vigne"), Coeur.valueOf("Dragon"), 27.3);
-		Baguette rH = new Baguette(Bois.valueOf("Chene"), Coeur.valueOf("Dragon"), 40.0);
+		
+		Baguette hG = new Baguette(Bois.VIGNE, Coeur.DRAGON, 27.3);
+		Baguette rH = new Baguette(Bois.CHENE, Coeur.DRAGON, 40.0);
 
 		daoB.insert(hG);
 		daoB.insert(rH);
 		System.out.println(hG);
 		System.out.println(rH);
+		
 	}
 
-//N'affiche pas la valeur de l'enum
+//OK
 	public static void selectBaguetteById() {
 
-		System.out.println("BOIS : " + daoB.selectById(1).getBois() + ", COEUR : " + daoB.selectById(1).getCoeur()
+		System.out.println("BOIS : " + daoB.selectById(1).getBois().getType () + ", COEUR : " + daoB.selectById(1).getCoeur().getMateriau()
 				+ ", TAILLE : " + daoB.selectById(1).getTaille());
+		System.out.println("BOIS : " + daoB.selectById(2).getBois().getType () + ", COEUR : " + daoB.selectById(2).getCoeur().getMateriau()
+				+ ", TAILLE : " + daoB.selectById(2).getTaille());
 	}
-
+//OK
 	public static void selectAllBaguette() {
 		for (Baguette b : daoB.selectAll()) {
 			System.out.println(b);
 		}
 	}
 
+//OK
 	public static void deleteBaguette() {
-		daoB.delete(daoB.selectById(1));
+		daoB.delete(daoB.selectById(3));
 	}
 }
