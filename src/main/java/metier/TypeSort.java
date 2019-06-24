16dbf4ddae3f8d2fc225f71f7b39b5f32f53ce64
amaly-paranavithana <amaly.paranavithana@laposte.net>
2019-06-24 17:13:30 +0200

    Enum TypeSort OK

--- src/main/java/metier/TypeSort.java
+++ src/main/java/metier/TypeSort.java
@@ -1,5 +1,18 @@
 package metier;
 
 public enum TypeSort {
+	ENCHANTEMENT("Enchantement"), MALEDICTION("Malediction"), MALEFICE("Malefice"), SORTILEGE("Sortilege");
 
+//Attribut
+	private String type;
+	
+//Constructeur
+	private TypeSort(String t) {
+		type = t;
+	}
+	
+//Getter
+	public String getType() {
+		return type;
+	}
 }
