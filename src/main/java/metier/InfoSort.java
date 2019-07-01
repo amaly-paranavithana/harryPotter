1343d6573be7f125571265670880703cd204485e
amaly-paranavithana <amaly.paranavithana@laposte.net>
2019-07-01 13:29:12 +0200

    modification de la classe Sort et InfoSort en fonction de la nouvelle structure d'InfoSort

--- src/main/java/metier/InfoSort.java
+++ src/main/java/metier/InfoSort.java
@@ -1,17 +1,35 @@
 package metier;
 
-public enum TypeSort {
-	ENCHANTEMENT("Enchantement"), MALEDICTION("Malediction"), MALEFICE("Malefice"), SORTILEGE("Sortilege");
+public enum InfoSort {
+	EAU("Aguamenti", "Enchantement"), AMNESIE("Oubliettes", "Enchantement"), ATTRACTION("Accio", "Enchantement"),
+	BOUCLIER("Protego", "Enchantement"), CHATOUILLIS("Rictusempra", "Enchantement"), LIMACE("Limacius Eructo", "Enchantement"),
+	DESARMEMENT("Expelliarmus", "Enchantement"), DEVERROUILLAGE("Alohomora", "ENchantement"), ENGORGEMENT("Amplificatum","Enchantement"),
+	EXPULSION("Repulso", "Enchantement"), LEVITATION("Wingardium Leviosa", "Enchantement"), LOCOMOTION("Locomotor", "Enchantement"),
+	MUTISME("Silencio","Enchantement"), PATRONUS("Spero Patronum", "Enchantement"), RECURAGE("Recurvite", "Enchantement"),
+	
+	DOLORIS("Endoloris", "Malediciton"), FURONCULOSE("Furunculus", "Malediction"), GEMINO("Gemino", "Malediction"), IMPERIUM("Impero", "Malediction"),
+	LANGUEDEPLOMB("Mutismus","Malediciton"), MORT("Avada Kedavra", "Malediction"), REDUCTION("Reducto", "Malediction"), SAUCISSON("Petrificus Totalus", "Malediction"),
+	PRINCE("Sectumsempra", "Malediction"),
+	
+	POTDECOLLE("Colloshoo", "Malefice"), BLOQUEJAMBES("Locomotor Mortis","Malefice"), EXPLOSIF("Confringo","Malefice"), ENTRAVE("Impedimenta", "Malefice");
+		
 
 //Attribut
+	private String formule;
 	private String type;
 	
 //Constructeur
-	private TypeSort(String t) {
+	private InfoSort(String f, String t) {
+		formule = f;
 		type = t;
 	}
 	
-//Getter
+//Getters
+	
+	public String getFormule() {
+		return formule;
+	}
+	
 	public String getType() {
 		return type;
 	}
--- src/main/java/metier/Sort.java
+++ src/main/java/metier/Sort.java
@@ -14,10 +14,13 @@ public class Sort {
 	private Integer id;
 	@Column(name = "nom")
 	private String nom;
-	@Column(name = "typeSort")
+	@Column(name = "formule")
 	@Enumerated(EnumType.STRING)
-	private TypeSort typeSort;
-	
+	private InfoSort formule;
+	@Column(name = "type")
+	@Enumerated(EnumType.STRING)
+	private InfoSort type;
+
 	@ManyToOne
 	private Matiere matiere;
 
@@ -25,20 +28,24 @@ public class Sort {
 	public Sort() {
 	}
 
-	public Sort(String nom, TypeSort typeSort) {
+	public Sort(String nom, InfoSort formule, InfoSort type) {
 		super();
 		this.nom = nom;
-		this.typeSort = typeSort;
+		this.formule = formule;
+		this.type = type;
 	}
 
-	public Sort(Integer id, String nom, TypeSort typeSort) {
+	public Sort(Integer id, String nom, InfoSort formule, InfoSort type, Matiere matiere) {
 		super();
 		this.id = id;
 		this.nom = nom;
-		this.typeSort = typeSort;
+		this.formule = formule;
+		this.type = type;
+		this.matiere = matiere;
 	}
 
-//Getters & Setters
+	// Getters & Setters
+	
 	public Integer getId() {
 		return id;
 	}
@@ -55,26 +62,45 @@ public class Sort {
 		this.nom = nom;
 	}
 
-	public TypeSort getTypeSort() {
-		return typeSort;
+	public InfoSort getFormule() {
+		return formule;
 	}
 
-	public void setTypeSort(TypeSort typeSort) {
-		this.typeSort = typeSort;
+	public void setFormule(InfoSort formule) {
+		this.formule = formule;
+	}
+
+	public InfoSort getType() {
+		return type;
+	}
+
+	public void setType(InfoSort type) {
+		this.type = type;
+	}
+
+	public Matiere getMatiere() {
+		return matiere;
+	}
+
+	public void setMatiere(Matiere matiere) {
+		this.matiere = matiere;
 	}
 
 //ToString
 	public String toString() {
-		return "Sort num. " + id + " : nom : " + nom + ", type de sort : " + typeSort;
+		return "Sort num. " + id + " : nom : " + nom + ", formule : " + formule + ", type de sort : " + type;
 	}
 
 //HashCode & Equals
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((formule == null) ? 0 : formule.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((matiere == null) ? 0 : matiere.hashCode());
 		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
-		result = prime * result + ((typeSort == null) ? 0 : typeSort.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
 		return result;
 	}
 
@@ -87,22 +113,26 @@ public class Sort {
 		if (getClass() != obj.getClass())
 			return false;
 		Sort other = (Sort) obj;
+		if (formule != other.formule)
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
+			return false;
+		if (matiere == null) {
+			if (other.matiere != null)
+				return false;
+		} else if (!matiere.equals(other.matiere))
 			return false;
 		if (nom == null) {
 			if (other.nom != null)
 				return false;
 		} else if (!nom.equals(other.nom))
 			return false;
-		if (typeSort == null) {
-			if (other.typeSort != null)
-				return false;
-		} else if (!typeSort.equals(other.typeSort))
+		if (type != other.type)
 			return false;
 		return true;
 	}
-
 }
+	
